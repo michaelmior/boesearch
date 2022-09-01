518a15f09850d5a6f37fade7667b955f9cb992d3
Michael Mior <mmior@mail.rit.edu>
2022-09-01 07:23:29 -0400

    Add missing Loader

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import { PuffLoader } from "react-spinners";
+
+function Loader() {
+  return <PuffLoader
+    color='#233E90'
+    cssOverride={{margin: '2em auto'}}
+  />
+}
+
+export default Loader;
